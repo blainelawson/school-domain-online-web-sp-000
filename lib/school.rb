28397945e8b4d6ccd65c87f35cb069cb5aaa2305
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-12 18:14:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,16 @@
 class School
-
-  attr_reader :name, :roster
+  attr_reader :name, :roster, :student_name, :grade
 
   def initialize(name, roster = {})
     @name = name
     @roster = roster
+  end
+
+  def add_student=(name, grade)
+    @student_name = name
+    @grade = grade
+    roster[grade] = []
+    roster[grade] << name
   end
 
 end
